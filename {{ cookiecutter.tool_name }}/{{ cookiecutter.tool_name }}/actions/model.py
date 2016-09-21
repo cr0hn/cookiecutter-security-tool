6b3582030316bd413e82f59c3b1c4a8486588c51
cr0hn <cr0hn@cr0hn.com>
2016-09-21 15:13:05 +0200

    spell changes

--- {{ cookiecutter.tool_name }}/{{ cookiecutter.tool_name }}/actions/model.py
+++ {{ cookiecutter.tool_name }}/{{ cookiecutter.tool_name }}/actions/model.py
@@ -5,6 +5,6 @@ from {{ cookiecutter.tool_name }} import SharedConfig, String
 class {{ cookiecutter.tool_name | capitalize }}Model(SharedConfig):
     target = String()
     
-__all__ = ("{{ cookiecutter.tool_name }}Model", )
+__all__ = ("{{ cookiecutter.tool_name | capitalize }}Model", )
 
 
